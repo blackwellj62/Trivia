ea41a40c46939c0a1019cb0b25a091d4636edf8b
Joey Blackwell <blackwellj6241@gmail.com>
2025-08-28 11:50:42 -0500

    changed answer model and updated database

--- Models/Answers.cs
+++ Models/Answers.cs
@@ -4,6 +4,7 @@ public class Answer
 {
     public int Id { get; set; }
     public string Text { get; set; }
+    public bool IsCorrect { get; set; }
 
     // Foreign key to Question
     public int QuestionId { get; set; }
