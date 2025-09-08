b33952c2e569ffa866303b3faabfc0c41bf27f10
Joey Blackwell <blackwellj6241@gmail.com>
2025-09-08 10:07:15 -0500

    added category select to homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,30 @@
 import TriviaShmivia from "/src/assets/TriviaShmiviaLogo.png"
 import "./Home.css"
+import { useEffect, useState } from "react"
+import { getCategories } from "../managers/categoryManager.js"
 
 export const Home = () => {
+const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+        getCategories().then(categoryArray=>{
+            setCategories(categoryArray)
+        })
+    },[])
+
     return(
+        <>
         <div className="home-container">
             <img src={TriviaShmivia} alt="Trivia Shmivia Logo" className="home-logo"/>
         </div>
+        <div>
+         <select className="category-menu">
+            <option value="0">Choose a Category</option>
+            {categories.map(category =>
+                <option value={category.id}>{category.name}</option>
+            )}
+         </select>
+        </div>
+        </>
     )
 }
