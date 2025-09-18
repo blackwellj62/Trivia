f3e4d62b747565dd4b3737ba84e0a975ea86ce32
Joey Blackwell <blackwellj6241@gmail.com>
2025-09-18 10:58:41 -0500

    added useEffect to load questions into state

--- client/src/components/Quiz/Quiz.jsx
+++ client/src/components/Quiz/Quiz.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom"
+import { getQuestions } from "../../managers/questionsManager.js";
 
 export const Quiz = () => {
 const [searchParams] = useSearchParams();
 const categoryId = searchParams.get("category")
 const [questions, setQuestions] = useState([])
-const [currentIndex, setCurrentIndex] = useState({})
-const [score, setScore] = useState("0")
-const [loading, setLoading] = useState("false")
-const [selectedAnswer, setSelectedAnswer] = useState({})
+// const [currentIndex, setCurrentIndex] = useState({})
+// const [score, setScore] = useState("0")
+// const [loading, setLoading] = useState("false")
+// const [selectedAnswer, setSelectedAnswer] = useState({})
 
+useEffect(()=>{
+   getQuestions(categoryId).then(data=>{
+    const questObj = data
+    setQuestions(questObj)
+   }) 
+},[categoryId])
 
 
 
