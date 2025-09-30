976ff3b45e796e20a4bc60176756cd8552498f64
Joey Blackwell <blackwellj6241@gmail.com>
2025-09-30 13:37:19 -0500

    installed clerk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = "pk_test_c3VwcmVtZS1zYXdmaXNoLTcwLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  </ClerkProvider>
 )
