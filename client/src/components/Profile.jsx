de55b2e7748c9d614271af1e7c5f45c720398973
Joey Blackwell <blackwellj6241@gmail.com>
2025-11-17 16:52:41 -0600

    added basic profile page

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,5 +1,18 @@
+import { useUser } from "@clerk/clerk-react"
+import { useState, useEffect } from "react"
+
+
+
 export const Profile = () => {
+    const {user, setUser} = useUser()
+
+   
     return(
-        <h2>User Profile Will Go Here</h2>
+        <div className="profile-container">
+            <h1>Profile</h1>
+            <h2>User Information:</h2>
+            <p>Name: {user.fullName || "No Name Set"}</p>
+            <p>Email: {user.primaryEmailAddress?.emailAddress}</p>
+        </div>
     )
 }
